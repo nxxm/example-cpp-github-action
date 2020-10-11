b8196240817e8e95b9b3446b967549fe172e1008
Lambourl <65184664+Lambourl@users.noreply.github.com>
2020-10-11 15:50:11 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+
+  std::cout << "😉 Welcome into a relaxing C++ experience !" << std::endl;
+  std::cout << "😉 Bienvenue dans une expérience C++ relaxante !" << std::endl;
+  std::cout << "😉 Willkommen zu einer entspannenden C++ Erfahrung !" << std::endl;
+  return 0;
+}
